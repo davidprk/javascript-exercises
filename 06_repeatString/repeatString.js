e57af7e453f29b1f76a452b5ee75d6fa0567b95c
David Park <daviddpark.dev@gmail.com>
2025-07-24 14:19:43 -0500

    Foundations, JS exercises

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, repeat) {
+    let newString = "";
 
+    if (repeat < 0) {
+        return "ERROR";
+    }
+    
+    for (let i = 0; i < repeat; i++) {
+        newString = newString.concat(string);
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let total = 0;
+    for (let i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+        total += i;
+    }
 
+    return total;
 };
 
 // Do not edit below this line
