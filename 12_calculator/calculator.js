ae603448b912ac91dc3fb709ce361b6234c7155f
David Park <daviddpark.dev@gmail.com>
2025-07-28 10:54:29 -0500

    Last exercises

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, curr) => total + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, curr) => total * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let res = a;
+  for (let i = a - 1; i > 1; i--) {
+    res *= i;
+  }
+
+  if (a > 1) {
+    return res;
+  }
+  else {
+    return 1;
+  }
 };
 
 // Do not edit below this line
